1ede94368a318a182c1a1c35ca4ab9d5b896e614
Administrator <Administrator@172.31.13.30>
2024-03-11 07:33:28 +0000

    dropdown

--- Google01/src/main/java/googleSearch/verify.java
+++ Google01/src/main/java/googleSearch/verify.java
@@ -1,10 +1,41 @@
 package googleSearch;
 
+import java.time.Duration;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
 public class verify {
 
-	public static void main(String[] args) {
-		System.out.println("Test");
-		System.out.println("Test");
+	public static void main(String[] args)  {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
+		
+		try
+		{
+			driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+			WebElement radioButton = driver.findElement(By.id("my-radio-2"));
+			Boolean isSelected = radioButton.isSelected();
+			System.out.println(isSelected);
+			
+			WebElement dropdown = driver.findElement(By.name("my-select"));
+			Select s = new Select(dropdown);
+		//	s.selectByVisibleText("One");
+		
+			List<WebElement> optionList = s.getOptions();
+			for(WebElement options : optionList)
+			System.out.println(options.getText());
+		}
+		finally
+		{
+			//driver.quit();
+		}
+		
 	}
 
 }
